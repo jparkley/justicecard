abee05957ec310ddae8bee3aa7402c8157278f80
Jjin SoL <parkleydondon@yahoo.com>
2021-03-23 00:22:52 -0700

    Finished Inertia server/client side setup

---
+++ public/resources_js_Pages_Home_index_tsx.js
@@ -0,0 +1,32 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_Home_index_tsx"],{
+
+/***/ "./resources/js/Pages/Home/index.tsx":
+/*!*******************************************!*\
+  !*** ./resources/js/Pages/Home/index.tsx ***!
+  \*******************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+
+var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var Homepage = function Homepage() {
+  return react_1["default"].createElement("p", null, "Welcome to Home page");
+};
+
+exports.default = Homepage;
+
+/***/ })
+
+}]);
---
+++ resources/js/Pages/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+const Homepage = () => {
+  return <p>Welcome to Home page</p>
+}
+export default Homepage
