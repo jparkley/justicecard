160e98117d75496c8a8e9758fc1e161ac9ebf9f8
Jjin SoL <parkleydondon@yahoo.com>
2021-03-22 23:20:52 -0700

    Added Laravel UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Jin SoL',
+            'email' => 'parkleydondon@yahoo.com',
+            'password' => \bcrypt('wjtmRkdchs0)'),
+        ]);
     }
 }
